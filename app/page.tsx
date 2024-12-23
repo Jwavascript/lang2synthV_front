614a0e50027dfe8f5112e623c3fc23210b52b9be
jwavascript <jwavascript@gmail.com>
2024-12-23 16:34:03 +0900

    [fix] : api address -> dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -36,8 +36,10 @@ export default function IPAConverter() {
     setLoading(true); // 로딩 시작
 
     try {
+      const apiUrl = `${process.env.NEXT_PUBLIC_API_BASE_URL}/convert`;
+
       // 백엔드 API 요청
-      const response = await fetch("http://127.0.0.1:8000/convert", {
+      const response = await fetch(apiUrl, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ text: input }),
