5eebc7c5327130114d45da56620063f66da43f50
jwavascript <jwavascript@gmail.com>
2024-12-25 16:40:30 +0900

    [add] loader text add

--- app/components/Loader.tsx
+++ app/components/Loader.tsx
@@ -3,6 +3,7 @@ import React from "react";
 const Loader: React.FC = () => (
   <div className="flex justify-center items-center">
     <div className="loader">Loading...</div>
+    <div className="loader">The first request can take ~1 minute.</div>
   </div>
 );
 
